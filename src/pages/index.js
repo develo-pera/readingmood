ddd0652b05d7dd39e0af9cabb62c3e9013c5bf8e
Petar Popovic <petarpnp@gmail.com>
2023-01-15 13:16:17 +0100

    Add og tags

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,6 +13,9 @@ export default function Home() {
         <title>{title}</title>
         <meta name="description" content={description} />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta property="og:title" content={title} />
+        <meta property="og:decription" content={description} />
+        <meta property="og:image" content="/og-image.jpg" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
