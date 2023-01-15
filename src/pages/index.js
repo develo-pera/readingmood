3ccd3e97c0d12a100779175e0394260fc5ab0a3e
Petar Popovic <petarpnp@gmail.com>
2023-01-15 17:24:33 +0100

    Add Twitter meta tags

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,6 +16,11 @@ export default function Home() {
         <meta property="og:title" content={title} />
         <meta property="og:decription" content={description} />
         <meta property="og:image" content="/og-image.jpg" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content={title} />
+        <meta name="twitter:description" content={description} />
+        <meta name="twitter:image" content="/og-image.jpg" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
