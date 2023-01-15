6b2d8719c71d95beb40d473cbef2c7d0bd4df99c
Petar Popovic <petarpnp@gmail.com>
2023-01-15 18:04:29 +0100

    Try to fix twitter og image

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,7 @@ import Playlist from "@/components/playlist/playlist";
 export default function Home() {
   const title = "Readingmood";
   const description = "Tell us what you're reading and we'll give you list of songs that goes along with the theme of the book and vice-versa.";
+
   return (
     <>
       <Head>
@@ -20,7 +21,7 @@ export default function Home() {
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content={title} />
         <meta name="twitter:description" content={description} />
-        <meta name="twitter:image" content="/og-image.jpg" />
+        <meta name="twitter:image" content={`${window.location.hostname}/og-image.jpg`} />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
