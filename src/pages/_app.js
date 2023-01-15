f67daed426a159f05a39ab84a676e9911224f18e
Petar Popovic <petarpnp@gmail.com>
2023-01-15 22:02:59 +0100

    Add google analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,3 +1,5 @@
+import Script from "next/script";
+
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "@/styles/main.scss";
@@ -5,6 +7,18 @@ import "@/styles/main.scss";
 export default function ReadingmoodApp({ Component, pageProps }) {
   return (
     <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-NHZJYG55N2"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}  
+            gtag('js', new Date());
+            gtag('config', 'G-NHZJYG55N2');
+          `}
+      </Script>
       <Component {...pageProps} />
       <ToastContainer
         className="ReadingmoodNotification"
