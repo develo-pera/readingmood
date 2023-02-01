58408572645cbac7219770424cefeadfb8748c4c
Petar Popovic <petarpnp@gmail.com>
2023-02-01 15:09:25 +0100

    Final commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,32 +1,12 @@
 import Script from "next/script";
-import LogRocket from "logrocket";
-import setupLogRocketReact from "logrocket-react";
 
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "@/styles/main.scss";
 
-if (typeof window !== "undefined") {
-  LogRocket.init('w7yj3j/readingmood');
-  // plugins should also only be initialized when in the browser
-  setupLogRocketReact(LogRocket);
-}
-
 export default function ReadingmoodApp({ Component, pageProps }) {
   return (
     <>
-      <Script
-        src="https://www.googletagmanager.com/gtag/js?id=G-NHZJYG55N2"
-        strategy="afterInteractive"
-      />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}  
-            gtag('js', new Date());
-            gtag('config', 'G-NHZJYG55N2');
-          `}
-      </Script>
       <Component {...pageProps} />
       <ToastContainer
         className="ReadingmoodNotification"
